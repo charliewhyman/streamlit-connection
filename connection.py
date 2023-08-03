8df9ca8a52b802d4e6ac83ede94645ae0d73becb
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2023-08-03 23:48:04 +0100

    Added connection and main streamlit app

--- connection.py
+++ connection.py
@@ -1,13 +1,75 @@
 from streamlit.connections import ExperimentalBaseConnection
+from streamlit import cache_data
+import requests
 
 # https://docs.streamlit.io/library/advanced-features/connecting-to-data#connection-building-best-practices
-class london_datastore_connection(ExperimentalBaseConnection):
+class tfl_api_connection(ExperimentalBaseConnection[requests.Session]):
 
-    def _connect(self, **kwargs) -> None:
+    def __init__(self, connection_name: str, **kwargs) -> None:
+        super().__init__(connection_name=connection_name, **kwargs)
+        self._session = self._connect(**kwargs)
+
+    # Implement _connect method, returning underlying requests.Session object. Requires app key from kwargs.
+    def _connect(self, **kwargs) -> requests.Session:
         if 'app_key' in kwargs:
             self.app_key = kwargs["app_key"]
-           
+        
         else:
-            raise Exception("Enter API key!")
+            raise Exception("Enter API Key")
+        
+        return requests.Session()
+    
+    # retrievers underlying connection object (requests.Session)
+    def cursor(self):
+        return self._session
+        
+    # helper query
+    def query(self, mode, ttl: int = 3600):
+        
+        @cache_data(ttl=ttl)
+        # for a given mode of transport, return a list of dictionaries containing line statuses.    
+        def get_mode_status(mode):
+            params = {
+                "app_key": self.app_key
+            }
+            
+            # https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_StatusByMode
+            endpoint = f'https://api.tfl.gov.uk/line/mode/{mode}/status'
+            
+            response = self._session.get(endpoint, params=params)
 
-        return None
+            if response.status_code != 200:
+                raise Exception(f"Error {response.status_code} - failed to get data.")
+            
+            else:
+                statuses = []
+
+                statusData = response.json()
+
+                # add every line/status combination to the list
+                for line in statusData:
+                    line_name = line['name']
+
+                    line_statuses = line['lineStatuses']
+
+                    for line_status in line_statuses:
+                        statusSeverity = line_status['statusSeverity']
+                        statusSeverityDescription = line_status['statusSeverityDescription']
+
+                        # statusSeverity of 10 == 'Good Service' - no need to return a status reason
+                        if line_status['statusSeverity'] != 10:
+                            reason = line_status['reason']
+
+                        else:
+                            reason = None
+
+                        status = {'line_name': line_name,
+                                  'statusSeverity': statusSeverity,
+                                'statusSeverityDescription': statusSeverityDescription,
+                                'reason': reason
+                                }
+                        statuses.append(status)
+
+            return statuses
+        
+        return get_mode_status(mode)
--- tfl_streamlit.py
+++ tfl_streamlit.py
@@ -1,14 +1,5 @@
 import streamlit as st
-from connection import london_datastore_connection
-import pandas as pd
-
-#populate modes dropdown /Line/Meta/Modes
-#user selects mode
-# populate available lines using /Line/Line_GetByMode
-#populate line dropdown
-#user selects line
-# Populate disruption card using /Line/Line_Disruption
-
+from connection import tfl_api_connection
 
 def main():
 
@@ -19,6 +10,36 @@ def main():
 
     # Ask user for API key
     app_key = st.text_input("Enter your TfL API Key", type="password")
+
+    # ask user to select a mode
+    modes = ['tube', 'dlr', 'overground', 'tram', 'elizabeth-line']
+
+    mode = st.selectbox('Select a mode of transport', modes)
+
+    # Create a button to get data from the TfL API
+    if st.button("Check status"):
+        if not app_key or not mode:
+            st.warning("Please enter Tfl API key and a mode.")
+        else:
+            conn = tfl_api_connection(connection_name='tfl_api_connection', app_key=app_key)
+
+            statuses = conn.query(mode)
+
+            if statuses == None:
+                st.error("Unable to connect to TfL API")
+            else:
+                display_mode_statuses(statuses)
+
+# 
+def display_mode_statuses(statuses):
+    for line in statuses:
+        st.subheader(line['line_name'])
+
+        st.markdown(f'**{line["statusSeverityDescription"]}**')
+
+        # no need to display reason if Good Service on the line
+        if line['reason'] != None:
+            st.text(line['reason'])
 
 if __name__ == "__main__":
     main()
